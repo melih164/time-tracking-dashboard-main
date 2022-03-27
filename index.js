65a0556b36b410dc547dfdaef6da462a929b706f
melih164 <melih3724@gmail.com>
2022-03-27 20:45:47 +0200

    grid system ,js dom manupulation

---
+++ index.js
@@ -0,0 +1,65 @@
+$(".daily").click(function () {
+
+    $(".workHour").text("5hrs");
+    $(".prevWorkHour").text("Previous - 7hrs");
+
+    $(".playHour").text("1hr");
+    $(".prevPlayHour").text("Previous - 2hrs");
+
+    $(".studyHour").text("0hrs");
+    $(".prevStudyHour").text("Previous - 1hr");
+
+    $(".exerciseHour").text("1hr");
+    $(".prevExerciseHour").text("Previous - 1hr");
+
+    $(".socialHour").text("1hr");
+    $(".prevSocialHour").text("Previous - 3hr");
+
+    $(".selfcareHour").text("0hr");
+    $(".prevSelfcareHour").text("Previous - 1hr");
+
+})
+
+$(".weekly").click(function () {
+
+    $(".workHour").text("32hrs");
+    $(".prevWorkHour").text("Previous - 36hrs");
+
+    $(".playHour").text("10hrs");
+    $(".prevPlayHour").text("Previous - 8hrs");
+
+    $(".studyHour").text("4hrs");
+    $(".prevStudyHour").text("Previous - 7hrs");
+
+    $(".exerciseHour").text("4hrs");
+    $(".prevExerciseHour").text("Previous - 5hrs");
+
+    $(".socialHour").text("5hrs");
+    $(".prevSocialHour").text("Previous - 10hrs");
+
+    $(".selfcareHour").text("2hrs");
+    $(".prevSelfcareHour").text("Previous - 2hrs");
+
+})
+
+$(".mounthly").click(function () {
+
+    $(".workHour").text("103hrs");
+    $(".prevWorkHour").text("Previous - 128hrs");
+
+    $(".playHour").text("23hrs");
+    $(".prevPlayHour").text("Previous - 29hrs");
+
+    $(".studyHour").text("13hrs");
+    $(".prevStudyHour").text("Previous - 19hrs");
+
+    $(".exerciseHour").text("11hrs");
+    $(".prevExerciseHour").text("Previous - 18hrs");
+
+    $(".socialHour").text("21hrs");
+    $(".prevSocialHour").text("Previous - 23hrs");
+
+    $(".selfcareHour").text("7hrs");
+    $(".prevSelfcareHour").text("Previous - 11hrs");
+
+})
